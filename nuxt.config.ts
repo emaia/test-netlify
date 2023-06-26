764a37de5b3f1edaf9b9627d68a92c6d283f1efd
Ednilson Maia <dina@veda.com.br>
2023-06-26 20:57:12 -0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Montserrat: [400, 600, 700, 900],
+    }
+  }
+})
